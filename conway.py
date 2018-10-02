78197d3158eaf880c82b3a365aae23d694f3d0da
Dmitry Tarasev <>
2018-10-02 00:17:56 -0400

    Program first commit

---
+++ conway.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import argparse
+from copy import deepcopy
+
+
+class Conway:
+
+    def main(self):
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument('iterations', type=int, help='Number of iterations')
+        parser.add_argument('filename', help='Input file containing starting state')
+
+        args = parser.parse_args()
+
+        conway_grid = []
+
+        for line in open(args.filename, 'r'):
+            conway_grid.append(list(line.strip()))
+
+        self.draw_grid(conway_grid)
+
+        for x in range(args.iterations):
+            conway_grid = self.iterate_grid(conway_grid)
+            self.draw_grid(conway_grid)
+
+    @staticmethod
+    def draw_grid(conway_grid):
+        for line in conway_grid:
+            print(line)
+        print("**************************************")
+
+    @staticmethod
+    def iterate_grid(g):
+
+        new_conway_grid = deepcopy(g)
+
+        x_len = len(g[0])
+        y_len = len(g)
+
+        for y, line in enumerate(g):
+            for x, char in enumerate(line):
+
+                alive = int(g[(x - 1) % x_len][(y - 1) % y_len]) + \
+                        int(g[(x - 1) % x_len][y]) + \
+                        int(g[(x - 1) % x_len][(y + 1) % y_len]) + \
+                        int(g[x][(y - 1) % y_len]) + \
+                        int(g[x][(y + 1) % y_len]) + \
+                        int(g[(x + 1) % x_len][(y - 1) % y_len]) + \
+                        int(g[(x + 1) % x_len][y]) + \
+                        int(g[(x + 1) % x_len][(y + 1) % y_len])
+
+                if g[x][y] is '1':
+                    if alive > 3 or alive < 2:
+                        new_conway_grid[x][y] = '0'
+                else:
+                    if alive is 3:
+                        new_conway_grid[x][y] = '1'
+
+        return new_conway_grid
+
+
+if __name__ == "__main__":
+    con = Conway()
+    con.main()
